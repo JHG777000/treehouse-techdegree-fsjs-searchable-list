aca8a15c4f0473c72295939212cad5fd302b02d0
JHG777000 <jhg777000@gmail.com>
2020-02-21 12:44:34 -0500

    add showSearchBar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # treehouse-techdegree-fsjs-project-2
+
+Displays a searchable list of students with page navigation, will reset search results if nothing is inputed in the search bar.
--- js/script.js
+++ js/script.js
@@ -3,70 +3,62 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+//The lis of all of the students.
 const students = document.getElementsByClassName('student-item cf');
+//Max number of items per page.
 const max_items_per_page = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+showPage(students,1); //Show the first page.
+appendPageLinks(students); //Add the page links.
+showSearchBar(); //Add the search bar.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+/*
+   The showPage function, shows a page of 'max_items_per_page' items 
+   for a given list.
+*/
 
 function showPage(list,page) {
 
+
+   //Get the start and end index for a given page.
    let start_index = (page*max_items_per_page)-max_items_per_page;
    let end_index = page*max_items_per_page;
 
-   for (let i = 0; i < students.length;i++) {
-      students[i].hidden = (i < end_index && i >= start_index ) ? false : true;
+   //Use the start and end index to hide all nonactive list elements,
+   //and unhide all active ones.
+   for (let i = 0; i < list.length;i++) {
+      list[i].hidden = (i < end_index && i >= start_index) ? false : true;
    }
 
 }
 
-showPage(students,1);
-appendPageLinks(students);
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+/*
+   The appendPageLinks function, displays page links for a given list,
+   allowing page navigation.
+*/
 
 function appendPageLinks(list) {
 
+   //Get, and create the DOM elements for the page links.
    let page_div = document.getElementsByClassName('page')[0];
-   let mydiv = document.createElement('div');
-   mydiv.className = 'pagination';
-   page_div.appendChild(mydiv);
-   let mylist = document.createElement('ul');
-   mydiv.appendChild(mylist);
+
+   let div = document.createElement('div');
+   div.className = 'pagination';
+   page_div.appendChild(div);
+
+   let list_of_elements = document.createElement('ul');
+   div.appendChild(list_of_elements);
+
    let number_of_pages = list.length / 10 ;
 
+   /*
+     The page_link_clicked function, responds to a click event on a page link
+     and changes the page, as well sets the page link to active and the last
+     one to nonactive.
+   */
+
    function page_link_clicked(e) {
-      let page_list = mylist.getElementsByTagName('a');
+      let page_list = list_of_elements.getElementsByTagName('a');
       let page_id = 0;
       for (let i = 0; i < page_list.length;i++) {
 
@@ -80,25 +72,98 @@ function appendPageLinks(list) {
          }
         
       }
-      showPage(students,page_id);
+      showPage(list,page_id);
    }
+
+   /*
+     The add_a_element function, adds an 'a' element to a page link.
+   */
 
    function add_a_element(li,page) {
       let element = document.createElement('a');
       element.href = '#';
       element.textContent = page;
-      if (page == 1) element.className = 'active';
-      element.addEventListener("click", page_link_clicked);
+      if (page === 1) element.className = 'active';
+      element.addEventListener('click', page_link_clicked);
       li.appendChild(element);
    }
 
    for (let i = 0; i < number_of_pages;i++) {
       let element = document.createElement('li');
       add_a_element(element,i+1);
-      mylist.appendChild(element);
+      list_of_elements.appendChild(element);
    }
 }
 
-//.addEventListener("click", printQuote, false);
+/*
+   The showSearchBar function, displays a search bar,
+   allowing the search of the students list.
+*/
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+function showSearchBar() {
+   let page_header = document.getElementsByClassName('page-header cf')[0];
+
+   let div = document.createElement('div');
+   div.className = 'student-search';
+   page_header.appendChild(div);
+
+   let input = document.createElement('input');
+   input.placeholder = 'Search for students...';
+   input.addEventListener('keyup',search_students);
+   div.appendChild(input);
+
+   let button = document.createElement('button');
+   button.textContent = 'Search';
+   button.addEventListener('click',search_students);
+   div.appendChild(button);
+   
+  /*
+     The search_students function, searches the studens' list to find and 
+     display matches, organized in pages.
+   */
+
+   function search_students() {
+     
+      let search_list = []; //List of search results.
+
+      for (let i = 0; i < students.length;i++) {
+         let student = students[i].getElementsByTagName('h3')[0].innerText;
+         if ( (input.value.length > 0) && 
+         (student.toLowerCase().includes(input.value.toLowerCase())) ) {
+
+            search_list.push(students[i]); //Match add a student to search_list.
+         }
+      }
+
+   //Remove page links, so new ones can be added.   
+   let pagination = document.getElementsByClassName('pagination')[0];
+   if (pagination !== undefined) pagination.parentNode.removeChild(pagination);
+
+   //Remove 'No students found.' message.   
+   let page_div = document.getElementsByClassName('page')[0];
+   let msg = page_div.getElementsByTagName('h1')[0];
+   if (msg !== undefined) msg.parentNode.removeChild(msg);
+
+   //If nothing is in the search bar, reset to the full list of students.
+   if (input.value.length === 0) {
+      showPage(students,1);
+      appendPageLinks(students);
+      return;
+   }
+   //Show page '0', to reset the students list.
+   showPage(students,0);
+   if (search_list.length > 0) {
+      //Dispay pages for search result.
+      showPage(search_list,1);
+      appendPageLinks(search_list);
+   } else {
+      let page_div = document.getElementsByClassName('page')[0];
+      let msg = document.createElement('h1');
+      msg.textContent = 'No students found.';
+      page_div.appendChild(msg);
+   }
+
+   }
+}
+
+ 
